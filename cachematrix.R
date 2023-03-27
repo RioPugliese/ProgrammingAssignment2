954c20e1107e5a2655ffa267edb34b8c87bedc3f
RioPugliese <101741897+RioPugliese@users.noreply.github.com>
2023-03-27 14:20:59 +0100

    Update cachematrix.R
    Lorenzo Valerio PUGLIESE solution to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# -----
+# This function receives an input and, on the top of that, builds all the functions to create and get a matrix 
+# and to calculate and get its inverse. Then, it stores everything into a list and returns it.
+makeCacheMatrix <- function(mt=matrix()){
+    
+    # Reset the initial value of the inverse matrix
+    invMtx <- NULL
+    
+    # Set the matrix
+    setMtx <- function(given_mt){
+        mt <<- given_mt
+        invMtx <<- NULL
+    }
+    
+    # Get the matrix
+    getMtx <- function(){mt}
+    
+    # Set the inverse value of the matrix
+    setInverseMtx <- function(inverse){invMtx <<- inverse}
+    
+    # Get the inverse value of the matrix
+    getInverseMtx <- function(){invMtx}
+    
+    # Return all results in a list
+    list(setMtx=setMtx,
+         getMtx=getMtx,
+         setInverseMtx=setInverseMtx, 
+         getInverseMtx=getInverseMtx)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# -----
+# This function checks weather an inverse of the provided matrix already exists (i.e., invMtx != NULL). 
+# If that's the case, then it returns that cached value. 
+# Otherwise, it calculates it and then returns the new value.
+cacheSolve <- function(mt, ...){
+    
+    # Get the existing value of the inverse matrix
+    invMtx <- mt$getInverseMtx()
+    
+    # If the existing value is not null, then use this one
+    if(!is.null(invMtx)){
+        message("Reusing cached value")
+        return(invMtx)
+    }
+    
+    # Else: 1. get the matrix...
+    mtx <- mt$getMtx()
+    
+    # ... 2. Calculate its inverse value...
+    invMtx <- solve(mtx, ...)
+    
+    # ... 3. And store it in the object
+    mt$setInverseMtx(invMtx)
+    
+    # Return the value
+    invMtx
 }
+
+# ======
+# Checks
+my_mtx <- makeCacheMatrix(matrix(1:4,nrow =2,ncol = 2))
+my_mtx$getMtx()
+my_mtx$getInverseMtx()
+cacheSolve(my_mtx)
+
